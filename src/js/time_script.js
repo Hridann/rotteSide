74cd5deefb5e8308e88941378a45219255fedd83
avalds <avalds@gmail.com>
2015-09-16 18:05:14 +0200

    Added a hilarius title generator

--- src/js/time_script.js
+++ src/js/time_script.js
@@ -1,9 +1,25 @@
 $(document).ready(function() { 
-
-  var timer = setInterval(function() {
-	myTimer()
-  }, 100);
-  function myTimer() {
-	$('#time').html("" + new Date());
-  }
+	var titles = ["Hero", "Genius", "Legend", "Idiot", "Shit", "Ape", "Mastermind", "Knight", "King", "Winner", "Clown", "Teacher"];
+	var cause = ["of Justice", "of Evil", "of Pranks", "of Stupidity", "of Power", "of the Dead"];
+	var timer = setInterval(function() {
+		myTimer()
+	}, 100);
+	function myTimer() {
+		$('#time').html("" + new Date());
+	}
+	$("#knight_me", "body").click(function (){
+ 	 	navn = $("input[name=Name]").val();
+  	if(navn === ""){
+			navn = "Retard"
+  	}
+  	if(Math.random() < 0.5){
+			$("#free_title_div", "body").append($("<h2 class='bg-success mye_luft'>"+navn+" the " + titles[Math.floor(Math.random()*titles.length)] + "  " + cause[Math.floor(Math.random()*cause.length)] + ".</h2>"));
+		}
+		else{
+			if(Math.random() < 0.5){
+				$("#free_title_div", "body").append($("<h2 class='bg-success mye_luft'>"+navn+" the " + titles[Math.floor(Math.random()*titles.length)] +".</h2>"));
+			}
+		}
+		return false;
+  	});
 });
