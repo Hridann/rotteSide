9f6d7b93c8985235c03e3e973903118e43e904bd
lax1n <august.haug@gmail.com>
2015-09-16 18:27:30 +0200

    Augusts stuff

---
+++ awesome/js/basic.js
@@ -0,0 +1,20 @@
+$(function(){
+	
+	var img_life;
+	
+	var init = function(){
+		setHTMLObjects();
+		setStartUp();
+		setEvents();
+		
+		function setHTMLObjects(){
+			img_life = $("#img_life");
+		};
+		
+		function setStartUp(){
+		};
+		
+		function setEvents(){
+		};
+	}();
+});
