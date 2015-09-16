0e609514b73f9ea1872239d80346c3ce05158fc7
avalds <avalds@gmail.com>
2015-09-16 18:22:43 +0200

    added more titles and causes

--- src/js/time_script.js
+++ src/js/time_script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() { 
-	var titles = ["Hero", "Genius", "Legend", "Idiot", "Shit", "Ape", "Mastermind", "Knight", "King", "Winner", "Clown", "Teacher"];
-	var cause = ["of Justice", "of Evil", "of Pranks", "of Stupidity", "of Power", "of the Dead"];
+	var titles = ["Hero", "Genius", "Legend", "Idiot", "Shit", "Ape", "Mastermind", "Knight", "King", "Winner", "Clown", "Teacher", "Human", "God", "Messia", "Prophet", "Prisoner", "Survivor", "Predator", "Loser", "Disciple", "Warden"];
+	var cause = ["of Justice", "of Evil", "of Pranks", "of Stupidity", "of Power", "of the Dead", "of Earth", "of the Milkyway", "from Mars", "of Good"];
 	var timer = setInterval(function() {
 		myTimer()
 	}, 100);
